7efbfcc7006b84073180e50e394a7fcbccdfefde
omfarak <farukakomer@gmail.com>
2024-08-07 13:34:24 +0200

    7 aug
    part two

---
+++ src/main/java/adminNew/temelEkran/controller/RootController.java
@@ -0,0 +1,14 @@
+package adminNew.temelEkran.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/student/studentWelcome";
+    }
+}
